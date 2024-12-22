97b03340127918f021297dd9ac794586c898c4bd
SamVader <margarita.samorodova@gmail.com>
2024-12-22 23:18:06 +0300

    add playlist

--- playlist.py
+++ playlist.py
@@ -6,11 +6,13 @@ from http.client import InvalidURL
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def is_valid_playlist_url(url):
-    pattern = r'^https://music\.yandex\.ru/users/([a-zA-Z0-9!"#$%&()*+,\-./:;<=>?@\[\\\]^_`{|}~]+)/playlists/(\d+)(\?utm_medium=copy_link)?$'
-    match = re.match(pattern, url)
+    pattern1 = r'^https://music\.yandex\.ru/users/([a-zA-Z0-9!"#$%&()*+,\-./:;<=>?@\[\\\]^_`{|}~]+)/playlists/(\d+)(\?utm_medium=copy_link)?$'
+    pattern2 = r'^https://next\.music\.yandex\.ru/playlists/([a-f0-9\-]+)(\?utm_medium=copy_link)?$'
+    match1 = re.match(pattern1, url)
+    match2 = re.match(pattern2, url)
     if "yandex.ru" not in url:
         raise InvalidURL("Ссылка на плейлист не с Яндекс музыки")
-    if match:
+    if match1 or match2:
         return True
     else:
         raise InvalidURL("Некорректный формат ссылки")
@@ -34,4 +36,3 @@ def get_playlist_artists(playlist_url, token, count):
     except Exception as e:
         logging.error(f"Ошибка при получении артистов из плейлиста: {e}")
         raise
-
